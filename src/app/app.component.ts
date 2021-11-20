ca147e812c4b0f1d9a7391f344252fe0ea2c8474
Esther Leñador <e.lenadorc@hotmail.com>
2021-11-20 19:16:14 +0100

    data-binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'weatherapp';
+
+  cityName = 'app';
+
+  public searchCity (cityName:string): void {
+    this.cityName = cityName;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,42 @@
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WeatherCardComponent } from './weather-card/weather-card.component';
+import { WeatherForecastComponent } from './weather-forecast/weather-forecast.component';
+import { WeatherSearchComponent } from './weather-search/weather-search.component';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherCardComponent,
+    WeatherForecastComponent,
+    WeatherSearchComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatCardModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatGridListModule,
+    MatProgressSpinnerModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
